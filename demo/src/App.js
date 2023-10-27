5d98f57a7b459e1d37413264e88c98f6b0560d2a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-10-27 21:00:34 +0530

    subfunction

--- demo/src/App.js
+++ demo/src/App.js
@@ -10,13 +10,7 @@ const add = ()=>{
   return c;
 }
 
-const sub = ()=>{
-  let p = 85;
-  const q = 35;
-  let r = p - q;
-  return r;
 
-}
 
 function App() {
 let arr = [10,20,30,40,50,60,70,80,90];
@@ -29,8 +23,6 @@ let arr1 = arr.map((i)=>{
       <header className="App-header">
         <h3>my first react demo</h3>
         <h4>Addition is :- {add()}</h4>
-        <h4>Subtraction is :- {sub()}</h4>
-
         <h4>Array is :- {arr [2]}</h4>
         <ul>
           {arr1}
