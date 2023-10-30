aaa4dfb5b8aba7c8100356c05e89f2a9474e4f4e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-10-30 21:22:27 +0530

    fragment

--- demo/src/components/Firstdemo.js
+++ demo/src/components/Firstdemo.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Firstdemo = () =>{
       return (
-        <>
+      
         <div>
             <h3>My First Components called</h3>
             <h5>welcome ReactJS</h5>
         </div>
-        </>
+        
       );
 }
 
