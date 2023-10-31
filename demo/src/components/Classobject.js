a4c9e9962d224839ebf03d6763d227a40627bed8
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-10-31 13:59:25 +0530

    arrayofobject

--- demo/src/components/Classobject.js
+++ demo/src/components/Classobject.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 class Classobject extends Component {
     render() {
-        const arr = ["jay","raj","jaydip"]
-        const [a,b,c] = arr
+        const arr = ["jay","raj","jaydip","abc","pqr","xyz"]
+        const [a,b,...c] = arr // spreading (...)
+        const arr1 = [10,20,30]
+        const arr2 = [...arr1, ...arr] // .Merging 
         return (
             <div>
-                <h3>A is -- {a}</h3>
+                {/* <h3>A is -- {a}</h3>
                 <h3>B is -- {b}</h3>
-                <h3>C is -- {c}</h3>
+                <h3>C is -- {c}</h3> */}
+                <h3>Array list</h3>
+                <ul>
+                    {
+                        arr2.map((i)=>{
+                            return <li>{i}</li>
+                        })
+                    }
+                </ul>
             </div>
         );
     }
