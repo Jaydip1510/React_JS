c1e7801f7aab12d46cc7d0a491edaeea6d618641
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-11-03 22:04:42 +0530

    arrow

--- project1/src/components/ArrayComponent.jsx
+++ project1/src/components/ArrayComponent.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const ArrayComponent = () => {
-    let arr = [11,22,33,44,55]
+    let arr = [11,22,33,44,55];
+    let arr2 = [10,20,30,40,50];
+    let arr3 = [11,22,33,44,55];
+    let array = [...arr2,...arr3];
+
+    let year = [2001,2005,2012,2020]
+    const tot = arr2.reduce((total,arr2) => total + arr2)
     const result = arr.filter ((i) =>{
-        
+         return i == 44;
     })
 
     return (
@@ -15,7 +21,47 @@ const ArrayComponent = () => {
                     return <li>{i}</li>
                   })
                 }
-               </ul>
+               </ul><hr/>
+               <h5>Array Sum</h5>
+               Array Sum is :- {tot} <hr />
+               <h5>
+                   <ul>
+                    {result} is found.
+                   </ul>
+               </h5><hr />
+               <h5>Even Array List</h5>
+               <ul>
+                {
+                  arr3.map((i)=>{
+                    return i % 2 ? '' :<li>{i}</li>
+                  })
+                }
+               </ul><hr/>
+               <h5>Odd Array List</h5>
+               <ul>
+                {
+                  arr3.map((i)=>{
+                    return i % 2 ? <li>{i}</li>:''
+                  })
+                }
+               </ul><hr/>
+               <h5>Leap Year</h5>
+               <ul>
+                {
+                  year.map((i)=>{
+                    return i % 4 ? '' :<li>{i}</li>
+                  })
+                }
+               </ul><hr/>
+               <h5>Marge Array</h5>
+               <ul>
+                    {
+                        array.map((i)=>{
+                            return <li>{i}</li>
+                        })
+                    }
+                </ul>
+
        </div>
     )
 }
