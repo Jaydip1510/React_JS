4fbdc22e06d0271f60dee995e77325a6e1ebb494
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-11-22 16:53:26 +0530

    propsexample

--- demo/src/components/Props.jsx
+++ demo/src/components/Props.jsx
@@ -3,10 +3,27 @@ import PropsChild from "./PropsChild";
 
 const PropsExample = () => {
     const msg = "Hello Props"
+    const user = {
+        name:"jaydip",
+        age:26,
+        gender:"male",
+    }
+    const arrDetails = [
+        {
+         color:'blue',
+         data:"abc",
+         status:false
+       },
+       {
+        color:'green',
+        data:"pqr",
+        status:true
+       }
+   ]
      return (
         <div>
             <h3>Parent Components called...</h3>
-            <PropsChild message={msg} />
+            <PropsChild message={msg} userInfo ={user} arrInfo ={arrDetails} />
         </div>
      )
 }
--- demo/src/components/PropsChild.jsx
+++ demo/src/components/PropsChild.jsx
@@ -4,6 +4,14 @@ const PropsChild = (props) => {
     return (
         <div>
              <h3>Child Components called...{props.message}</h3>
+             <h4>Name is :- {props.userInfo.name}</h4>
+             <h4>age is :- {props.userInfo.age}</h4>
+             <h4>gender is :- {props.userInfo.gender}</h4>
+             {
+                props.arrInfo.map((i)=>{
+                    return <li>{i.color}</li>
+                })
+             }
         </div>
     )
 }
