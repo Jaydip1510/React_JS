8fbe97fb06aacc74c651c3b11a54f608387ea2c7
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-11-30 16:51:14 +0530

    carousel

--- boostrapreacrt-demo/src/App.js
+++ boostrapreacrt-demo/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Carouselcomponent from './components/Carouselcomponent';
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       
+       <Carouselcomponent/>
       </header>
     </div>
   );
