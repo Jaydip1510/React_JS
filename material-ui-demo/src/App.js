64e76a439b96eba3e606c3fdcb91c329c365a278
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-11-30 16:31:57 +0530

    materialui

--- material-ui-demo/src/App.js
+++ material-ui-demo/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Radiocomponent from './components/Radiocomponent';
-
+import Textcomponent from './components/Textcomponent';
+import Selectcomponent from './components/Selectcomponent';
+import Transferlist from './components/Transferlist';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Radiocomponent/>
+        {/* <Radiocomponent/><hr />
+        <Textcomponent/>
+        <Selectcomponent/> */}
+        <Transferlist />
+        
       </header>
     </div>
   );
