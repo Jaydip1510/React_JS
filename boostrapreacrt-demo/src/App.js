44a5e923bd1ce5e789221698f4e3d1b41074cf92
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-11-30 21:00:17 +0530

    validation

--- boostrapreacrt-demo/src/App.js
+++ boostrapreacrt-demo/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Buttongroupcomponent from './components/Buttongroupcomponent';
+import Validationcomponent from './components/Validationcomponent';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       
-       <Buttongroupcomponent/>
+       {/* <Buttongroupcomponent/> */}
+       <Validationcomponent/>
       </header>
     </div>
   );
