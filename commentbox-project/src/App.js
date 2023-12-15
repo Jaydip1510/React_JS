48f5b92a965c4ca770df4d9ce44770b3cae3c0a4
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-12-15 19:49:20 +0530

    changes

--- commentbox-project/src/App.js
+++ commentbox-project/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Commentbox from './components/Commentbox';
-
 function App() {
   return (
     <div className="App">
--- commentbox-project/src/components/Commentbox.jsx
+++ commentbox-project/src/components/Commentbox.jsx
@@ -1,66 +1,48 @@
 import React, { useState } from 'react';
 
 const Commentbox = () => {
-    const [name, setName] = useState('');
-    const [comment, setComment] = useState('');
-    const [errors, setErrors] = useState({});
+  const [comment, setComment] = useState('');
+  const [commentsList, setCommentsList] = useState([]);
 
-    const validateForm = () => {
-        const errors = {};
-        if (!name.trim()) {
-            errors.name = 'Name is required';
-        }
-        if (!comment.trim()) {
-            errors.comment = 'Comment is required';
-        }
-        return errors;
-    };
+  const handleInputChange = (event) => {
+    setComment(event.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const validationErrors = validateForm();
-        if (Object.keys(validationErrors).length === 0) {
-            console.log('Name:', name);
-            console.log('Comment:', comment);
-            // Clear form fields
-            setName('');
-            setComment('');
-            setErrors({});
-        } else {
-            setErrors(validationErrors);
-        }
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (comment.trim() !== '') {
+      setCommentsList([...commentsList, comment]);
+      setComment('');
+    }
+  };
 
-    return (
-        <div>
-            <h2>Comment Box</h2>
-            <form onSubmit={handleSubmit}>
-                <table border={2} align='center'  style={{height:200, width:300}}>
-                    <div  className='mt-3'  >
-                        <label style={{ color: 'black', width: 100 }} htmlFor="">Name:-</label>
-                        <input
-                           
-                            type="text"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        {errors.name && <p className="error">{errors.name}</p>}
-                    </div><br />
-                    <div>
-                        <label style={{ color: 'black' }} htmlFor="">Comment:-</label>
-                        <textarea
-                            id="comment"
-                            value={comment}
-                            onChange={(e) => setComment(e.target.value)}
-                        ></textarea>
-                        {errors.comment && <p className="error">{errors.comment}</p>}
-                    </div>
-                    <button type="submit">Submit</button>
-                </table>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit} style={{margin:50,}}>
+      <table border={2} align='center' className='mt-5' style={{height:250,width:250}}>
+        <textarea
+          placeholder="Write your comment here..."
+          value={comment}
+          onChange={handleInputChange}
+        ></textarea><br/><br/>
+        <button type="submit">Submit</button>
+        </table>
+      </form>
+
+      <div>
+        <h3>Comments</h3>
+        {commentsList.length === 0 ? (
+          <p>No comments yet.</p>
+        ) : (
+          <ul>
+            {commentsList.map((c, index) => (
+              <li key={index}>{c}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Commentbox;
