f5ff4e8928ad4e773c1a68a78c9352d7e6c5f2c1
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-12-18 14:35:34 +0530

    cssapply

--- demo/src/components/Localstoragedemo.jsx
+++ demo/src/components/Localstoragedemo.jsx
@@ -30,29 +30,29 @@ const Localstoragedemo = () => {
   return (
     <div className='App'>
       <form name="frm" onSubmit={submitData}>
-      <table border={2} align='center' cellPadding={0} cellSpacing={0} style={{height:500,width:600}} >
+      <table border={2} align='center' className='bg-secondary' cellPadding={0} cellSpacing={0} style={{height:500,width:600}} >
 
-        <label htmlFor="" style={{ marginRight: 10 }} className='mb-2'>Name:-</label>
+        <label htmlFor="" style={{ marginRight: 10,color:"blue",fontWeight:"bold" }} className='mb-2'>Name:-</label>
         <input type="text" className='mt-5' style={{height:35,width:200}} name="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Your Name' />
         <br/><br/> 
 
-        <label htmlFor="" style={{ marginRight: 25 }} className='mb-2'>Age:-</label>
+        <label htmlFor="" style={{ marginRight: 25,color:"blue",fontWeight:"bold" }} className='mb-2'>Age:-</label>
         <input type="text" name="age" style={{height:35,width:200}} value={age} onChange={(e)=>setAge(e.target.value)} placeholder='Enter Your Age' />
         <br/><br/> 
 
-        <label htmlFor="" style={{ marginRight: 10 }} className='mb-2'>Salary:-</label>
+        <label htmlFor="" style={{ marginRight: 10,color:"blue",fontWeight:"bold" }} className='mb-2'>Salary:-</label>
         <input type="text" name="salary" style={{height:35,width:200}} value={salary} onChange={(e)=>setSalary(e.target.value)} placeholder='Enter Your Salary' />
         <br/><br/> 
 
-        <label htmlFor="" style={{ marginRight: 15 }} className='mb-2'>Email:-</label>
+        <label htmlFor="" style={{ marginRight: 15,color:"blue",fontWeight:"bold" }} className='mb-2'>Email:-</label>
         <input type="email" name="email" style={{height:35,width:200}} value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Your Email' />
         <br/><br/> 
 
-        <label htmlFor="" style={{ marginRight: 25 }}>Add:-</label>
+        <label htmlFor="" style={{ marginRight: 25,color:"blue",fontWeight:"bold" }}>Add:-</label>
         <textarea name="address" cols="30" rows="2" style={{height:35,width:200}} value={address} onChange={(e)=>setAddress(e.target.value)} placeholder='Enter Your address' ></textarea>
         <br/><br/> 
 
-        <label htmlFor="">Qualification:-</label>
+        <label htmlFor="" style={{marginLeft:15,color:"blue",fontWeight:"bold" }}>Qualification:-</label>
         <input type="checkbox"  name="q1" value="10th" defaultChecked="" onChange={getQualData} />
         10th
         <input type="checkbox" name="q1" value="12th" defaultChecked="" onChange={getQualData} />
@@ -62,7 +62,7 @@ const Localstoragedemo = () => {
         <input type="checkbox" name="q1" value="MCA" defaultChecked="" onChange={getQualData} />
         MCA
         <br/><br/>
-        <button type="submit" className="btn btn-success">Submit</button>
+        <button type="submit" className="btn btn-warning">Submit</button>
         </table>
       </form>
     </div>
