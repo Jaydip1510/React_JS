97c41b4765306c5d372c93bd4c445b7784489872
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-12-23 15:08:25 +0530

    newproject

---
+++ localstorage/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Localstoragecomponent from './components/Localstoragecomponent';
+
+function App() {
+  return (
+    <div>
+      <Localstoragecomponent/>
+    </div>
+  );
+}
+
+export default App;
