52b823d3942066131c0566a379dc8075130d4684
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-12-23 15:15:00 +0530

    sessionstorage

--- localstorage/src/App.js
+++ localstorage/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Localstoragecomponent from './components/Localstoragecomponent';
+import Sessionstoragecomponent from './components/Sessionstoragecomponent';
 
 function App() {
   return (
     <div>
-      <Localstoragecomponent/>
+      {/* <Localstoragecomponent/> */}
+      <Sessionstoragecomponent/>
     </div>
   );
 }
