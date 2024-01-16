5c5a25e2478051642184096d09a667967abeee3e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-01-16 11:16:43 +0530

    changes

--- practicalexam-reactrouter/src/components/Form.jsx
+++ practicalexam-reactrouter/src/components/Form.jsx
@@ -1,49 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Form() {
-    const [input,setInput] = useState([]);
-    const [empdata,setEmpdata] = useState();
-    
-    const saveData = (e) => {
-        const name = e.name.value;
-        const value = e.value.value
-    }
+const Form = () => {
+  const [formData, setFormData] = useState({ name: '', age: '',g1:'', email: '', password: '' });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const saveData = (e) => {
+    e.preventDefault();
+    const storedData = JSON.parse(localStorage.getItem('empdata')) || [];
+    const updatedData = [...storedData, formData];
+    localStorage.setItem('empdata', JSON.stringify(updatedData));
+    setFormData({ name: '', age: '',g1:'', email: '', password: '' });
+    navigate('/table');
+  };
+
   return (
     <div>
       <h3>User Form</h3>
       <form onSubmit={saveData} method='post'>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        <br />
+        <br/>
 
-        <label htmlFor="">Name:-</label>
-        <input type="text" name="name" placeholder='Enter Your Name' />
+        <label htmlFor="age">Age:</label>
+        <input type="number" name="age" value={formData.age} onChange={handleChange} />
+        <br />
+        <br/>
+
+        <label htmlFor="">Gender:-</label>
+        <input type="radio" name="g1" id="" value="Male" onChange={handleChange}/>Male 
+        <input type="radio" name="g1" id="" value="Female" onChange={handleChange} />Female
         <br/>
         <br/>
-       
-       <label htmlFor="">Age:-</label>
-       <input type="number" name="age" placeholder='Enter Your Age' />
-       <br/>
-       <br/>
 
-       <label htmlFor="">Gender:-</label>
-       <input type="radio" name="g1"  value={'Male'} />Male&nbsp;&nbsp;
-       <input type="radio" name="g1"  value={'Female'} />Female
-       <br/>
-       <br/>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        <br />
+        <br/>
 
-       <label htmlFor="">Email:-</label>
-       <input type="email" name="email" placeholder='Enter Your Email' />
-       <br />
-       <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <br />
+        <br/>
 
-       <label htmlFor="">Password:-</label>
-       <input type="password" name="password" placeholder='Enter Your Password' />
-       <br/>
-       <br/>
-
-       <input type="button" value="Save" /> 
-
+        <button className='btn btn-warning' type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- practicalexam-reactrouter/src/components/Table.jsx
+++ practicalexam-reactrouter/src/components/Table.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function Table() {
+const Table = () => {
+  const [userdata, setUserdata] = useState([]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('empdata')) || [];
+    setUserdata(storedData);
+  }, []);
+
+  const deleteInfo = (index) => {
+    const updatedData = [...userdata];
+    updatedData.splice(index, 1);
+    setUserdata(updatedData);
+    localStorage.setItem('empdata', JSON.stringify(updatedData));
+  };
+  const searchData = (val) => {
+    if (val !== '') {
+      let dt = userdata.filter((i) => {
+        if (i.name.includes(val)) {
+          return i;
+        }
+      })
+      setUserdata(dt);
+    } else {
+      setUserdata(JSON.parse(localStorage.getItem('empdata')));
+   
+    }
+  }
   return (
     <div>
-      <h3>User Table</h3>
+      <h3><u>User Table</u></h3>
+      <input type="text" name='searchName' placeholder='Enter any want to search' onChange={(e) => searchData(e.target.value)} />
+      <br />
+      <br />
+      <table border={2} className='table table-striped'>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Gender</td>
+            <td>Email</td>
+            <td>Password</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {userdata.map((i, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{i.name}</td>
+              <td>{i.age}</td>
+              <td>{i.g1}</td>
+              <td>{i.email}</td>
+              <td>{i.password}</td>
+              <td>
+                <button className="btn btn-danger" onClick={() => deleteInfo(index)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
