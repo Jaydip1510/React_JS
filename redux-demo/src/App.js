03e74df6b468424dbd5cd415e1852d7568b85e04
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-01-24 12:38:30 +0530

    changes

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -4,14 +4,17 @@ import { Provider } from 'react-redux';
 import Store from './Store/Store';
 import Redux from './components/Redux';
 import Calculator from './components/Redux';
+import FirstComponent from './components/FirstComponent';
 
 function App() {
+  const name = "jaydip makwana"
   return (
     <div className="App">
-      <h3>Redux Couter</h3>
+      {/* <h3>Redux Couter</h3>
       <Provider store={Store}>
        <Redux/>
-      </Provider>
+      </Provider> */}
+      <FirstComponent name={name}/>
     </div>
   );
 }
---
+++ redux-demo/src/components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FirstComponent(props) {
+  return (
+    <div>
+        <h3>First Component</h3>
+        name is :- {props.name}
+    </div>
+  )
+}
+
+export default FirstComponent
