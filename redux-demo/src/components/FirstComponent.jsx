da224a1b4a3c6bec68ea616157c8e6d69bdce25a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-01-24 15:41:46 +0530

    changes

--- redux-demo/src/components/FirstComponent.jsx
+++ redux-demo/src/components/FirstComponent.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
-
+import React, { createContext, useContext } from 'react'
+const ct = createContext()
 function FirstComponent(props) {
   return (
     <div>
         <h3>First Component</h3>
-      <SecondComponent name={props.name}/>
+        <ct.Provider value={props.name}>
+          name is:- {props.name}
+          <FourComponent/>
+        </ct.Provider>
     </div>
   );
 }
-const SecondComponent = () => {
+const SecondComponent = (props) => {
     return (
         <div>
             <h3>Second Component</h3>
-           
+           <ThirdComponent name={props.name}/>
         </div>
       );
 }
 
-const Third
+const ThirdComponent = (props) => {
+  return (
+    <div>
+        <h3>Third Component</h3>
+       <FourComponent name={props.name}/>
+    </div>
+  );
+
+}
+
+const FourComponent = (props) => {
+  return (
+    <div>
+        <h3>Four Component</h3>
+          name={useContext(ct)}
+    </div>
+  );
+}
 
 export default FirstComponent
