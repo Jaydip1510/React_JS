74d9dba5312b2db339b69140bee574ae57e1a1bd
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-11 09:59:38 +0530

    storedata

--- reduxtoolkit_demo/src/redux/Store.jsx
+++ reduxtoolkit_demo/src/redux/Store.jsx
@@ -1,14 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import cntReducer from "./Reducers";
-
-
-// const store = configureStore({
-//   reducer: {
-//     cntData: cntReducer
-//   }
-// });
-
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
 import  useReducer  from './Reducers';
 
