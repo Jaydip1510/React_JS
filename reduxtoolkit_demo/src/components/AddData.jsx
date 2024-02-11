117a51f39f6f01b1d5015babdce2314c58a94457
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-11 11:14:42 +0530

    formstyle

--- reduxtoolkit_demo/src/components/AddData.jsx
+++ reduxtoolkit_demo/src/components/AddData.jsx
@@ -28,53 +28,61 @@ function AddData() {
     });
   };
   const deleteData = (id) => {
-     dispatch(deleteItem(id));
+    dispatch(deleteItem(id));
   }
   return (
     <div>
-       <form onSubmit={handleSubmit}>
-           <label>Name:-</label>
-           <input type="text" name="name" value={inputValue.name} onChange={handleChange} />
-           <br/><br/>
+      <div className='frm1'>
+        <div className='frm'>
+          <form onSubmit={handleSubmit} className='f1'>
+            <label>Name:-</label>
+            <input type="text" name="name" value={inputValue.name} onChange={handleChange} />
+            <br /><br />
 
-           <label>Age:-</label>
-           <input type="text" name="age" value={inputValue.age} onChange={handleChange} />
-           <br/><br/>
+            <label>Age:-</label>
+            <input type="text" name="age" value={inputValue.age} onChange={handleChange} />
+            <br /><br />
 
-           <label>Email:-</label>
-           <input type="email" name="email" value={inputValue.email} onChange={handleChange} />
-           <br/><br/>
+            <label>Email:-</label>
+            <input type="email" name="email" value={inputValue.email} onChange={handleChange} />
+            <br /><br />
 
-           <lable>Password:-</lable>
-           <input type="password" name="password" value={inputValue.password} onChange={handleChange} />
-           <br/><br/>
-           <button type="submit">SaveData</button>
-       </form>
-       <br/><br/>
-       <table border={3}>
-         <tr>
-             <td>Id</td>
-             <td>Name</td>
-             <td>Age</td>
-             <td>Email</td>
-             <td>Password</td>
-             <td>Action</td>
-         </tr>
-         {
-              userinfo.map((i,index) =>{
-                  return(
-                      <tr>
-                         <td>{index+1}</td>
-                         <td>{i.name}</td>
-                         <td>{i.age}</td>
-                         <td>{i.email}</td>
-                         <td>{i.password}</td>
-                         <td><button type='button' onClick={()=> deleteData(index)}>Delete</button></td>
-                      </tr>
-                  )
-              })
-         }
-       </table>
+            <lable>Password:-</lable>
+            <input type="password" name="password" value={inputValue.password} onChange={handleChange} />
+            <br /><br />
+            <button  class="btn btn-outline-success" type="submit">SaveData</button>
+          </form>
+        </div>
+      </div>
+      <br /><br />
+      <table border={3} className='table table-striped'>
+        <thead className='fw-bold'>
+          <tr textalign="center">
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+            <td>Password</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userinfo.map((i, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>{i.email}</td>
+                  <td>{i.password}</td>
+                  <td><button class="btn btn-outline-danger" type='button' onClick={() => deleteData(index)}>Delete</button></td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
