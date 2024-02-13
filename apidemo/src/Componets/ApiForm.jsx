209a7128fa206f97903039c8cad2ac92a6948718
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-13 17:24:48 +0530

    changes

--- apidemo/src/Componets/ApiForm.jsx
+++ apidemo/src/Componets/ApiForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ApiForm() {
      const [empdata,setEmpdata] = useState({
@@ -7,8 +7,11 @@ function ApiForm() {
         salary:"",
         address:"",
      });
-     const [id,setId] = useState("");
-
+     useEffect(() => {
+      fetch("http://localhost:3000/emp")
+        .then(res => res.json())
+        .then(json => setEmpdata(json));
+    });
      const handleChange = (e) => {
           const {name,value} = e.target;
           setEmpdata({...empdata,[name]:value});
@@ -28,22 +31,45 @@ function ApiForm() {
     <div>
            <form name="frm" onSubmit={setData} method='post'>
             <label>Name:-</label>
-            <input type="text" name="name" value={empdata.name} onChange={handleChange}/>
+            <input type="text" id='name' name="name" value={empdata.name} onChange={handleChange}/>
             <br /><br />
 
             <label htmlFor="">Age:-</label>
-            <input type="text" name="age" value={empdata.age} onChange={handleChange}/>
+            <input type="text" id='age' name="age" value={empdata.age} onChange={handleChange}/>
             <br /><br />
 
             <label htmlFor="">Salary:-</label>
-            <input type='text' name="salary" value={empdata.salary} onChange={handleChange}/>
+            <input type='text' id='salary' name="salary" value={empdata.salary} onChange={handleChange}/>
             <br /><br />
             
             <label htmlFor="">Address:-</label>
-            <input type="text" name="address" value={empdata.address} onChange={handleChange}/>
+            <input type="text" id='address' name="address" value={empdata.address} onChange={handleChange}/>
             <br/><br />
             <input type="submit" value={"Save"}/>
            </form>
+           <br/>
+           <br/>
+           <table>
+               <tr>
+                  <td>Id</td>
+                  <td>Name</td>
+                  <td>Age</td>
+                  <td>Salary</td>
+                  <td>Address</td>
+               </tr>
+               {
+                  empdata.map((i) => (
+                     <tr key={i.id}>
+                       <td>{i.id}</td>
+                       <td>{i.name}</td>
+                       <td>{i.age}</td>
+                       <td>{i.salary}</td>
+                       <td>{i.address}</td>
+                       <td><button>Delete</button></td>
+                     </tr>
+                   ))
+               }
+           </table>
     </div>
   )
 }
