73e0784339039e29d18e0b646599651170f80f7a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-14 16:23:28 +0530

    changes

--- apidemo/src/Componets/ApiForm.jsx
+++ apidemo/src/Componets/ApiForm.jsx
@@ -1,91 +1,97 @@
 import React, { useEffect, useState } from 'react'
 
 function ApiForm() {
-     const [dt,setDt] = useState([]);
-     const [empdata,setEmpdata] = useState({
-         name:"",
-         age:"",
-         salary:"",
-         address:""
-     });
-     useEffect(() => {
+   const [dt, setDt] = useState([]);
+   const [uid,setUid] = useState();
+   const [empdata, setEmpdata] = useState({
+      name: "",
+      age: "",
+      salary: "",
+      address: ""
+   });
+   useEffect(() => {
       fetch("http://localhost:3000/emp")
-        .then(res => res.json())
-        .then(json => setDt(json));
-        
-    });
-     const handleChange = (e) => {
-          const {name,value} = e.target;
-          setEmpdata({...empdata,[name]:value});
-     }
-     const setData = () => {
-        fetch("http://localhost:3000/emp",{
-          method: "POST",
-          headers:{
-             "Content-Type": "application/json"
-          },
-          body: JSON.stringify(empdata)
-        })
-           .then(res => res.json())
-           .then(json =>console.log(json));
-     }
-     const removeData = (id) => {
-      fetch(`http://localhost:3000/emp/${id}`,{
-         method: "DELETE",
-         headers:{
+         .then(res => res.json())
+         .then(json => setDt(json));
+
+   });
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      setEmpdata({ ...empdata, [name]: value });
+   }
+   const setData = () => {
+      fetch("http://localhost:3000/emp", {
+         method: "POST",
+         headers: {
             "Content-Type": "application/json"
          },
          body: JSON.stringify(empdata)
-       })
-          .then(res => res.json())
-          .then(json =>console.log(json));
-     }
-  return (
-    <div>
-           <form name="frm" onSubmit={setData} method='post'>
+      })
+         .then(res => res.json())
+         .then(json => console.log(json));
+   }
+   const removeData = (id) => {
+      fetch(`http://localhost:3000/emp/${id}`, {
+         method: "DELETE",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify(empdata)
+      })
+         .then(res => res.json())
+         .then(json => console.log(json));
+   }
+   const editData = (id) => {
+      fetch(`http://localhost:3000/emp/${id}`)
+         .then(res => res.json())
+         .then(json => setEmpdata(json));
+   }
+   return (
+      <div>
+         <form name="frm" onSubmit={setData} method='post'>
             <label>Name:-</label>
-            <input type="text" id='name' name="name" value={empdata.name} onChange={handleChange}/>
+            <input type="text" id='name' name="name" value={empdata.name} onChange={handleChange} />
             <br /><br />
 
             <label htmlFor="">Age:-</label>
-            <input type="text" id='age' name="age" value={empdata.age} onChange={handleChange}/>
+            <input type="text" id='age' name="age" value={empdata.age} onChange={handleChange} />
             <br /><br />
 
             <label htmlFor="">Salary:-</label>
-            <input type='text' id='salary' name="salary" value={empdata.salary} onChange={handleChange}/>
+            <input type='text' id='salary' name="salary" value={empdata.salary} onChange={handleChange} />
             <br /><br />
-            
+
             <label htmlFor="">Address:-</label>
-            <input type="text" id='address' name="address" value={empdata.address} onChange={handleChange}/>
-            <br/><br />
-            <input type="submit" value={"Save"}/>
-           </form>
-           <br/>
-           <br/>
-           <table border={2}>
-               <tr>
-                  <td>Id</td>
-                  <td>Name</td>
-                  <td>Age</td>
-                  <td>Salary</td>
-                  <td>Address</td>
-                  <td>Action</td>
-               </tr>
-               {
-                  dt.map((i) => (
-                     <tr>
-                       <td>{i.id}</td>
-                       <td>{i.name}</td>
-                       <td>{i.age}</td>
-                       <td>{i.salary}</td>
-                       <td>{i.address}</td>
-                       <td><button type='button' onClick={()=>removeData(i.id)}>Delete</button></td>
-                     </tr>
-                   ))
-               }
-           </table>
-    </div>
-  )
+            <input type="text" id='address' name="address" value={empdata.address} onChange={handleChange} />
+            <br /><br />
+            <input type="submit" value={"Save"} />
+         </form>
+         <br />
+         <br />
+         <table border={2}>
+            <tr>
+               <td>Id</td>
+               <td>Name</td>
+               <td>Age</td>
+               <td>Salary</td>
+               <td>Address</td>
+               <td>Action</td>
+            </tr>
+            {
+               dt.map((i) => (
+                  <tr>
+                     <td>{i.id}</td>
+                     <td>{i.name}</td>
+                     <td>{i.age}</td>
+                     <td>{i.salary}</td>
+                     <td>{i.address}</td>
+                     <td><button type='button' onClick={() => editData(i.id)}>Edit</button><button type='button' onClick={() => removeData(i.id)}>Delete</button></td>
+                  </tr>
+               ))
+            }
+         </table>
+      </div>
+   )
 }
 
 export default ApiForm
