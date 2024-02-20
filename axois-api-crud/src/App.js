6d5a92cde84bbd8bac0c60918a9149f08f21a868
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-20 18:04:17 +0530

    axoisapi

---
+++ axois-api-crud/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Apiform from './Components/Apiform';
+
+function App() {
+  return (
+    <div className="App">
+      <Apiform/>
+    </div>
+  );
+}
+
+export default App;
---
+++ axois-api-crud/src/Components/Apiform.jsx
@@ -0,0 +1,109 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+const Apiform = () => {
+    const [data,setData] = useState([])
+    const [userData,setUserData] = useState({
+        name:'',
+        age:'',
+        email:'',
+        password:''
+    })
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setUserData({...userData,[name]:value})
+    }
+    const fetchitem = async () => {
+        try {
+           await axios.get('http://localhost:3000/user').then
+           (res=>setData(res.data));
+        }catch(error){
+            console.log(error.message);
+        }
+     }
+    useEffect(() => {
+        // axios.get('http://localhost:3000/user')
+        //     .then((res) => setData(res.data))
+        //     .catch((error) => {
+        //         console.error('Error fetching data:', error);
+        //     });
+            fetchitem();
+    }, []);
+    const handleSubmmit = (e) => {
+        e.preventDefault();
+    
+        axios.post('http://localhost:3000/user', userData)
+            .then(res => {
+                console.log(res.data);
+                setUserData({
+                    name: '',
+                    age: '',
+                    email: '',
+                    password: ''
+                });
+                
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+            fetchitem();
+    }
+    const deleteData = (id) => {
+        axios.delete(`http://localhost:3000/user/${id}`)
+            .then(res => {
+                console.log(res.data); 
+            })
+            .catch(error => {
+                console.error('Error deleting data:', error);
+            });
+            fetchitem();
+    };
+  return (
+    <div>
+      <form onSubmit={handleSubmmit}>
+          <label>Name:-</label>
+          <input type="text" name='name' value={userData.name} onChange={handleChange} />
+          <br/><br/>
+
+          <label>Age:-</label>
+          <input type='text' name='age' value={userData.age} onChange={handleChange} />
+          <br/><br/>
+
+          <label>Email:-</label>
+          <input type='email' name='email' value={userData.email} onChange={handleChange} />
+          <br/><br/>
+
+          <label>Password:-</label>
+          <input type='password' name='password' value={userData.password} onChange={handleChange} />
+          <br/><br/>
+
+          <button type='submit'>SaveData</button>
+      </form>
+      <br/><br/>
+      <table border={2}>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+            <td>Password</td>
+            <td>Action</td>
+          </tr>
+          {
+              data.map((i) => (
+                    <tr>
+                        <td>{i.id}</td>
+                        <td>{i.name}</td>
+                        <td>{i.age}</td>
+                        <td>{i.email}</td>
+                        <td>{i.password}</td>
+                        <td><button type='button' onClick={() => deleteData(i.id)}>Delete</button></td>
+                    </tr>  
+              ))
+          }
+      </table>
+    </div>
+  )
+}
+
+export default Apiform
