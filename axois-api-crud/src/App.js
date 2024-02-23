526c060d136970a10d4dced0ddf4582e713fff51
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-23 12:05:22 +0530

    firebase

--- axois-api-crud/src/App.js
+++ axois-api-crud/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Apiform from './Components/Apiform';
 import Googlecomponent from './Components/Googlecomponent';
+import FirebaseComponents from './FirebaseComponents';
 
 function App() {
   return (
     <div>
-      <Googlecomponent/>
+      {/* <Googlecomponent/> */}
       {/* <h3 className='txt'>Axios CrudOperation</h3> */}
       {/* <Apiform/> */}
-      
+      <FirebaseComponents/>
     </div>
   );
 }
--- axois-api-crud/src/index.js
+++ axois-api-crud/src/index.js
@@ -5,15 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='44781859447-094plnjkec7nuponad0vrplsm0dk9ic1.apps.googleusercontent.com'>
-    <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
+   <App/>
+  </React.StrictMode>,
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
