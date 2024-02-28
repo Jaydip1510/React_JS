071902ae974444f03427ed432317f84356a9c21e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-28 12:27:56 +0530

    displaydata

--- axois-api-crud/src/EmployeeComponents.jsx
+++ axois-api-crud/src/EmployeeComponents.jsx
@@ -1,16 +1,37 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, doc, onSnapshot, query } from 'firebase/firestore';
 import { db } from './firebase/Demofirebase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function EmployeeComponents() {
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [salary, setSalary] = useState('');
+    const [inputValue,setInputValue] = useState({
+        name:'',
+        age:'',
+        email:'',
+        password:'',
+        salary:'',
+    })
+    const [allData,setAllData] = useState([]);
 
+    useEffect(() => {
+        const j = query(collection(db,'empData'));
+        const data = onSnapshot(j,(querySnapshot)=>{
+            let dataArray = [];
+            querySnapshot.forEach((doc)=>{
+                dataArray.push({...doc.data(),id: doc.id});
+            });
+            setAllData(dataArray);
+        })
+        return () => data;
+    },[]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({ ...inputValue, [name]: value });
+    }
     const handleSubmmit = (e) => {
         e.preventDefault();
+        const { name, age, email, password, salary } = inputValue;
+
         if (name !== '' && age !== '' && email !== '' && password !== '' && salary !== '') {
             addDoc(collection(db, "empData"), {
                 name,
@@ -20,45 +41,78 @@ function EmployeeComponents() {
                 salary,
                 completed: false,
             });
+
             // Reset all input fields
-            setName('');
-            setAge('');
-            setEmail('');
-            setPassword('');
-            setSalary('');
+            setInputValue({
+                name: '',
+                age: '',
+                email: '',
+                password: '',
+                salary: '',
+            });
         }
     }
+
 
     return (
         <div>
             <form onSubmit={handleSubmmit}>
                 <label>Name:-</label>
-                <input type='text' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type='text' name='name' value={inputValue.name} onChange={handleChange} />
                 <br />
                 <br />
 
                 <label>Age:-</label>
-                <input type='number' name='age' value={age} onChange={(e) => setAge(e.target.value)} />
+                <input type='number' name='age' value={inputValue.age} onChange={handleChange} />
                 <br />
                 <br />
 
                 <label>Email:-</label>
-                <input type='text' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type='text' name='email' value={inputValue.email} onChange={handleChange} />
                 <br />
                 <br />
 
                 <label>Password</label>
-                <input type='text' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type='text' name='password' value={inputValue.password} onChange={handleChange} />
                 <br />
                 <br />
 
                 <label>Salary:-</label>
-                <input type='number' name='salary' value={salary} onChange={(e) => setSalary(e.target.value)} />
+                <input type='number' name='salary' value={inputValue.salary} onChange={handleChange} />
                 <br />
                 <br />
 
                 <button type='submit'>Save Data</button>
             </form>
+            <br/><br/>
+            <table>
+                  <thead>
+                    <tr>
+                        <td>id</td>
+                        <td>Name</td>
+                        <td>Age</td>
+                        <td>Email</td>
+                        <td>Password</td>
+                        <td>Salary</td>
+                        <td>Action</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                        allData.map((i)=>{
+                          <tr>
+                            <td>{i.id}</td>
+                            <td>{i.name}</td>
+                            <td>{i.age}</td>
+                            <td>{i.email}</td>
+                            <td>{i.password}</td>
+                            <td>{i.salary}</td>
+                            <td><button>Delete</button></td>
+                          </tr>
+                        })
+                    }
+                  </tbody>
+            </table>
         </div>
     )
 }
