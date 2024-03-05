7eac09b6fb584bc8bc99e43763da87e15d2d7755
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-05 14:07:11 +0530

    googleauth

---
+++ google_auth/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import GoogleAuthcomponents from './Components/GoogleAuthcomponents';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <GoogleAuthcomponents/>
+    </div>
+  );
+}
+
+export default App;
