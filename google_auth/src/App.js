63c9d07858ab7eeb8b47abee35534db1e79de40f
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-05 14:08:37 +0530

    spaceremove

--- google_auth/src/App.js
+++ google_auth/src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import GoogleAuthcomponents from './Components/GoogleAuthcomponents';
 
-
-
-
 function App() {
   return (
     <div className="App">
