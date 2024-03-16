12681cba16f4aec1571d81b1ac1eea7f9f15bfce
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-16 21:12:46 +0530

    newfile

--- finalproject_reactjs/src/Components/Heder/Heder.jsx
+++ finalproject_reactjs/src/Components/Heder/Heder.jsx
@@ -1,172 +1,187 @@
 import React from 'react'
-import '../../lib/lightbox/css/lightbox.min.css';
-import '../../lib/owlcarousel/assets/owl.carousel.min.css';
-import '../../css/bootstrap.min.css';
+import '../../assets/img/favicon.png';
+import '../../assets/css/all.min.css';
+import '../../assets/bootstrap/css/bootstrap.min.css';
+import '../../assets/css/owl.carousel.css';
+import '../../assets/css/magnific-popup.css';
+import '../../assets/css/animate.css';
+import '../../assets/css/meanmenu.min.css';
+import '../../assets/css/main.css';
+import '../../assets/css/responsive.css';
 
 const Heder = () => {
-  return (
-    <div>
-        <>
-  {/* Spinner Start */}
-  <div
-    id="spinner"
-    className="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center"
-  >
-    <div className="spinner-grow text-primary" role="status" />
-  </div>
-  {/* Spinner End */}
-  {/* Navbar start */}
-  <div className="container-fluid fixed-top">
-    <div className="container topbar bg-primary d-none d-lg-block">
-      <div className="d-flex justify-content-between">
-        <div className="top-info ps-2">
-          <small className="me-3">
-            <i className="fas fa-map-marker-alt me-2 text-secondary" />{" "}
-            <a href="#" className="text-white">
-              123 Street, New York
-            </a>
-          </small>
-          <small className="me-3">
-            <i className="fas fa-envelope me-2 text-secondary" />
-            <a href="#" className="text-white">
-              Email@Example.com
-            </a>
-          </small>
-        </div>
-        <div className="top-link pe-2">
-          <a href="#" className="text-white">
-            <small className="text-white mx-2">Privacy Policy</small>/
-          </a>
-          <a href="#" className="text-white">
-            <small className="text-white mx-2">Terms of Use</small>/
-          </a>
-          <a href="#" className="text-white">
-            <small className="text-white ms-2">Sales and Refunds</small>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="container px-0">
-      <nav className="navbar navbar-light bg-white navbar-expand-xl">
-        <a href="index.html" className="navbar-brand">
-          <h1 className="text-primary display-6">Fruitables</h1>
-        </a>
-        <button
-          className="navbar-toggler py-2 px-3"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="fa fa-bars text-primary" />
-        </button>
-        <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
-          <div className="navbar-nav mx-auto">
-            <a href="index.html" className="nav-item nav-link active">
-              Home
-            </a>
-            <a href="shop.html" className="nav-item nav-link">
-              Shop
-            </a>
-            <a href="shop-detail.html" className="nav-item nav-link">
-              Shop Detail
-            </a>
-            <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Pages
-              </a>
-              <div className="dropdown-menu m-0 bg-secondary rounded-0">
-                <a href="cart.html" className="dropdown-item">
-                  Cart
-                </a>
-                <a href="chackout.html" className="dropdown-item">
-                  Chackout
-                </a>
-                <a href="testimonial.html" className="dropdown-item">
-                  Testimonial
-                </a>
-                <a href="404.html" className="dropdown-item">
-                  404 Page
-                </a>
-              </div>
-            </div>
-            <a href="contact.html" className="nav-item nav-link">
-              Contact
-            </a>
-          </div>
-          <div className="d-flex m-3 me-0">
-            <button
-              className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4"
-              data-bs-toggle="modal"
-              data-bs-target="#searchModal"
-            >
-              <i className="fas fa-search text-primary" />
-            </button>
-            <a href="#" className="position-relative me-4 my-auto">
-              <i className="fa fa-shopping-bag fa-2x" />
-              <span
-                className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1"
-                style={{ top: "-5px", left: 15, height: 20, minWidth: 20 }}
-              >
-                3
-              </span>
-            </a>
-            <a href="#" className="my-auto">
-              <i className="fas fa-user fa-2x" />
-            </a>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </div>
-  {/* Navbar End */}
-  {/* Modal Search Start */}
-  <div
-    className="modal fade"
-    id="searchModal"
-    tabIndex={-1}
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div className="modal-dialog modal-fullscreen">
-      <div className="modal-content rounded-0">
-        <div className="modal-header">
-          <h5 className="modal-title" id="exampleModalLabel">
-            Search by keyword
-          </h5>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          />
-        </div>
-        <div className="modal-body d-flex align-items-center">
-          <div className="input-group w-75 mx-auto d-flex">
-            <input
-              type="search"
-              className="form-control p-3"
-              placeholder="keywords"
-              aria-describedby="search-icon-1"
-            />
-            <span id="search-icon-1" className="input-group-text p-3">
-              <i className="fa fa-search" />
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  {/* Modal Search End */}
-  {/* Modal Search End */}
-</>
+    return (
+        <div>
+            <>
+                {/* Hello world */}
+                {/*PreLoader*/}
+                {/* <div className="loader">
+                    <div className="loader-inner">
+                        <div className="circle" />
+                    </div>
+                </div> */}
+                {/*PreLoader Ends*/}
+                {/* header */}
+                <div className="top-header-area" id="sticker">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12 col-sm-12 text-center">
+                                <div className="main-menu-wrap">
+                                    {/* logo */}
+                                    <div className="site-logo">
+                                        <a href="index.html">
+                                            <img src="assets/img/logo.png" alt="" />
+                                        </a>
+                                    </div>
+                                    {/* logo */}
+                                    {/* menu start */}
+                                    <nav className="main-menu">
+                                        <ul>
+                                            <li className="current-list-item">
+                                                <a href="#">Home</a>
+                                                <ul className="sub-menu">
+                                                    <li>
+                                                        <a href="index.html">Static Home</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="index_2.html">Slider Home</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="about.html">About</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Pages</a>
+                                                <ul className="sub-menu">
+                                                    <li>
+                                                        <a href="404.html">404 page</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="about.html">About</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="cart.html">Cart</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="checkout.html">Check Out</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="contact.html">Contact</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="news.html">News</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="shop.html">Shop</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="news.html">News</a>
+                                                <ul className="sub-menu">
+                                                    <li>
+                                                        <a href="news.html">News</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="single-news.html">Single News</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="contact.html">Contact</a>
+                                            </li>
+                                            <li>
+                                                <a href="shop.html">Shop</a>
+                                                <ul className="sub-menu">
+                                                    <li>
+                                                        <a href="shop.html">Shop</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="checkout.html">Check Out</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="single-product.html">Single Product</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="cart.html">Cart</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <div className="header-icons">
+                                                    <a className="shopping-cart" href="cart.html">
+                                                        <i className="fas fa-shopping-cart" />
+                                                    </a>
+                                                    <a className="mobile-hide search-bar-icon" href="#">
+                                                        <i className="fas fa-search" />
+                                                    </a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                    <a className="mobile-show search-bar-icon" href="#">
+                                        <i className="fas fa-search" />
+                                    </a>
+                                    <div className="mobile-menu" />
+                                    {/* menu end */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* end header */}
+                {/* search area */}
+                <div className="search-area">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <span className="close-btn">
+                                    <i className="fas fa-window-close" />
+                                </span>
+                                <div className="search-bar">
+                                    <div className="search-bar-tablecell">
+                                        <h3>Search For:</h3>
+                                        <input type="text" placeholder="Keywords" />
+                                        <button type="submit">
+                                            Search <i className="fas fa-search" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* end search area */}
+                {/* hero area */}
+                <div className="hero-area hero-bg">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-9 offset-lg-2 text-center">
+                                <div className="hero-text">
+                                    <div className="hero-text-tablecell">
+                                        <p className="subtitle">Fresh &amp; Organic</p>
+                                        <h1>Delicious Seasonal Fruits</h1>
+                                        <div className="hero-btns">
+                                            <a href="shop.html" className="boxed-btn">
+                                                Fruit Collection
+                                            </a>
+                                            <a href="contact.html" className="bordered-btn">
+                                                Contact Us
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* end hero area */}
+            </>
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Heder
