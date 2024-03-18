ba5b7fbae136207d54a349009fc4fcd9269baf45
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-18 16:52:37 +0530

    appspace

--- finalproject_reactjs/src/App.js
+++ finalproject_reactjs/src/App.js
@@ -1,13 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-// import Footer from './component/Footer';
 import NavBar from './component/NavBar';
-// import PopularProduct from './component/PopularProduct';
-// import ProductSession from './component/ProductSession';
-// import RecentBox from './component/RecentBox';
-// import Testimonials from './component/Testimonials';
-// import WeHelpYou from './component/WeHelpYou';
-// import WhyChoose from './component/WhyChoose';
 import Shop from './component/Shop';
 import AboutUs from './component/AboutUs';
 import Services from './component/Services';
@@ -15,16 +8,6 @@ import Services from './component/Services';
 function App() {
   return (
     <div>
-      
-      {/* <NavBar />
-      <ProductSession />
-      <WhyChoose />
-      <WeHelpYou />
-      <PopularProduct />
-      <Testimonials />
-      <RecentBox />
-      <Footer /> */}
-
       <Routes>
         <Route path='/' element={<NavBar/>} />
         <Route path='/shop' element={<Shop/>} />
