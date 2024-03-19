bb8d989e4ce1bd2eca45c1e7a52f0a5037ba935a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-19 14:54:52 +0530

    header

--- finalproject_reactjs/src/component/Heder.jsx
+++ finalproject_reactjs/src/component/Heder.jsx
@@ -5,9 +5,9 @@ import { Link, useLocation } from 'react-router-dom'
 
 const Heder = () => {
      const location = useLocation();
-  return (
-    <div>
-        <nav
+     return (
+          <div>
+               <nav
                     className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark"
                     arial-label="Furni navigation bar"
                >
@@ -27,38 +27,38 @@ const Heder = () => {
                               <span className="navbar-toggler-icon" />
                          </button>
                          <div className="collapse navbar-collapse" id="navbarsFurni">
-                         <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-      <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-      </li>
-      <li className={`nav-item ${location.pathname === '/shop' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/shop">
-          Shop
-        </Link>
-      </li>
-      <li className={`nav-item ${location.pathname === '/about' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/about">
-          About us
-        </Link>
-      </li>
-      <li className={`nav-item ${location.pathname === '/services' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/services">
-          Services
-        </Link>
-      </li>
-      <li className={`nav-item ${location.pathname === '/blog' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/blog">
-          Blog
-        </Link>
-      </li>
-      <li className={`nav-item ${location.pathname === '/contact' ? 'active' : ''}`}>
-        <Link className="nav-link" to="/contact">
-          Contact us
-        </Link>
-      </li>
-    </ul>
+                              <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                                   <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/">
+                                             Home
+                                        </Link>
+                                   </li>
+                                   <li className={`nav-item ${location.pathname === '/shop' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/shop">
+                                             Shop
+                                        </Link>
+                                   </li>
+                                   <li className={`nav-item ${location.pathname === '/about' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/about">
+                                             About us
+                                        </Link>
+                                   </li>
+                                   <li className={`nav-item ${location.pathname === '/services' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/services">
+                                             Services
+                                        </Link>
+                                   </li>
+                                   <li className={`nav-item ${location.pathname === '/blog' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/blog">
+                                             Blog
+                                        </Link>
+                                   </li>
+                                   <li className={`nav-item ${location.pathname === '/contact' ? 'active' : ''}`}>
+                                        <Link className="nav-link" to="/contact">
+                                             Contact us
+                                        </Link>
+                                   </li>
+                              </ul>
                               <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
                                    <li>
                                         <a className="nav-link" href="#">
@@ -74,8 +74,8 @@ const Heder = () => {
                          </div>
                     </div>
                </nav>
-    </div>
-  )
+          </div>
+     )
 }
 
 export default Heder
