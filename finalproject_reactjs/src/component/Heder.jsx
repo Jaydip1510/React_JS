22cf1d6f4646f4e74ca256c7aa4b108387190d0d
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-19 14:53:16 +0530

    selectpage

--- finalproject_reactjs/src/component/Heder.jsx
+++ finalproject_reactjs/src/component/Heder.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import user from '../images/user.svg'
 import cart from '../images/cart.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Heder = () => {
+     const location = useLocation();
   return (
     <div>
         <nav
@@ -26,38 +27,38 @@ const Heder = () => {
                               <span className="navbar-toggler-icon" />
                          </button>
                          <div className="collapse navbar-collapse" id="navbarsFurni">
-                              <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-                                   <li className="nav-item active">
-                                        <Link className="nav-link" to="/">
-                                             Home
-                                        </Link>
-                                   </li>
-                                   <li>
-                                        <Link className="nav-link" to="/shop">
-                                             Shop
-                                        </Link>
-                                   </li>
-                                   <li>
-                                        <Link className="nav-link" to="/about">
-                                             About us
-                                        </Link>
-                                   </li>
-                                   <li>
-                                        <Link className="nav-link" to="/services">
-                                             Services
-                                        </Link>
-                                   </li>
-                                   <li>
-                                        <Link className="nav-link" to="/blog">
-                                             Blog
-                                        </Link>
-                                   </li>
-                                   <li>
-                                        <Link className="nav-link" to="/contact">
-                                             Contact us
-                                        </Link>
-                                   </li>
-                              </ul>
+                         <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+      <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className={`nav-item ${location.pathname === '/shop' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/shop">
+          Shop
+        </Link>
+      </li>
+      <li className={`nav-item ${location.pathname === '/about' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/about">
+          About us
+        </Link>
+      </li>
+      <li className={`nav-item ${location.pathname === '/services' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/services">
+          Services
+        </Link>
+      </li>
+      <li className={`nav-item ${location.pathname === '/blog' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/blog">
+          Blog
+        </Link>
+      </li>
+      <li className={`nav-item ${location.pathname === '/contact' ? 'active' : ''}`}>
+        <Link className="nav-link" to="/contact">
+          Contact us
+        </Link>
+      </li>
+    </ul>
                               <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
                                    <li>
                                         <a className="nav-link" href="#">
