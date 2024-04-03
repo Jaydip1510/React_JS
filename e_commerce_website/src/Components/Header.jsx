cffd154bb2186e4456b8d0c6ab42618da83e028f
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-04-03 21:32:43 +0530

    changes

--- e_commerce_website/src/Components/Header.jsx
+++ e_commerce_website/src/Components/Header.jsx
@@ -579,6 +579,22 @@ const Header = () => {
       </div>
     </div>
   </div>
+  <div data-vide-bg="video/video">
+    <div className="container">
+      <div className="banner-info">
+        <h3>
+          It is a long established fact that a reader will be distracted by the
+          readable{" "}
+        </h3>
+        <div className="search-form">
+          <form action="#" method="post">
+            <input type="text" placeholder="Search..." name="Search..." />
+            <input type="submit" defaultValue=" " />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </>
   )
 }
